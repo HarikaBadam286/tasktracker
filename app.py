8b79efc1114c1371ba0475c1c4b2ac93f01a584a
Harika Badam <harika.badam@ericsson.com>
2026-04-22 12:26:27 +0200

    added Flask api

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-import sys
+from flask import Flask, request, jsonify
 import task_manager as tm
 
-cmd = sys.argv[1]
+app = Flask(__name__)
 
-if cmd == "add":
-    tm.add_task(sys.argv[2])
-elif cmd == "list":
-    tm.list_tasks()
-elif cmd == "delete":
-    tm.delete_task(sys.argv[2])
-elif cmd == "help":
-    tm.display_help()
-elif cmd == "done":
-    tm.mark_done(sys.argv[2])
-else:
-    print("Please enter valid option")
-    tm.display_help()
+@app.route("/")
+def home():
+    return "Hello!!! Welcome to Task Manager"
+
+@app.route("/tasks", methods = ["GET"])
+def get_tasks():
+    return jsonify(tm.load_tasks())
+
+@app.route("/tasks", methods = ["POST"])
+def add_tasks():
+    data = request.json
+    tm.add_task(data["task"])
+    return {"message": "Task added"}
+
+@app.route("/tasks/<int:id>", methods = ["DELETE"])
+def delete_task(id):
+    tm.delete_task(id)
+    return {"message": "Task deleted"}
+
+@app.route("/tasks/<int:id>", methods = ["PUT"])
+def mark_done(id):
+    tm.mark_done(id)
+    return {"message": "Task updated"}
+
+if __name__=="__main__":
+    app.run(debug=True)
